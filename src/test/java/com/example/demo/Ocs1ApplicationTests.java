6b6212534cf5339c61910b21d69c5aa1d60899c9
Abhilash4862 <you@example.com>
2022-05-05 19:26:03 +0530

    heelo

---
+++ src/test/java/com/example/demo/Ocs1ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes= Ocs1Application.class)
+//@SpringBootTest
+class Ocs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
+
